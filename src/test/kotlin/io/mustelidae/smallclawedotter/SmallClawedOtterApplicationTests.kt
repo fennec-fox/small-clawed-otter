60bae47485c0acc64669568ceae3cbe283974f98
fennec.fox <fennec.klar@kakaomobility.com>
2021-11-22 13:38:29 +0900

    Fix test

--- src/test/kotlin/io/mustelidae/smallclawedotter/SmallClawedOtterApplicationTests.kt
+++ src/test/kotlin/io/mustelidae/smallclawedotter/SmallClawedOtterApplicationTests.kt
@@ -2,7 +2,9 @@ package io.mustelidae.smallclawedotter
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("embedded")
 @SpringBootTest
 class SmallClawedOtterApplicationTests {
 
